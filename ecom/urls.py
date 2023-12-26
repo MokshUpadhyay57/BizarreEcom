ee78d0ec18737c8a0ab95f2f3bd29dc9b91a27f5
Moksh Upadhyay <mokshupadhyay5@gmail.com>
2023-12-26 15:39:02 +0530

    Make some changes

--- ecom/urls.py
+++ ecom/urls.py
@@ -7,5 +7,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('app.urls')),
+    path('',include('app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
